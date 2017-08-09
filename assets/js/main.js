58e7ae93b341858c4260c3a14f428c2105edd896
Mariley20 <mariley2018@gmail.com>
2017-08-09 10:27:32 -0500

    solo imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+for (var i = 0; i < pictures.length; i++) {
+	var tag_Li = document.createElement('img');
+	var content = document.createTextNode(pictures[i]);
+	tag_Li.appendChild(content);
+
+	picture.appendChild(tag_Li)
+}
