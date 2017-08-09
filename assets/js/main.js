f0d969ad66d284ba7d65015bf896902cf1fc3cad
Mariley20 <mariley2018@gmail.com>
2017-08-09 13:52:29 -0500

    terminado pictures

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
 for (var i = 0; i < pictures.length; i++) {
-	var tag_Li = document.createElement('img');
+	var tag_img = document.createElement('img');
 	
-	tag_Li.setAttribute('src', pictures[i]);
+	tag_img.setAttribute('src', pictures[i]);
+	tag_img.setAttribute('class', 'images')
 
-	picture.appendChild(tag_Li)
+
+	picture.appendChild(tag_img)
 }
